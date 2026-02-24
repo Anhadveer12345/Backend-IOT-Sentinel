a63db9bbea483a135d142701f1aa61a2872d2e88
Anhadveer Singh <anhadveersingh@Anhadveers-MacBook-Pro.local>
2026-02-24 22:55:19 +0530

    fix port and add gunicorn

--- api.py
+++ api.py
@@ -298,4 +298,5 @@ if __name__ == '__main__':
     print("\n[API] Starting on http://localhost:8080")
     print("[API] Press Ctrl+C to stop\n")
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=False)
+
+    app.run(host='0.0.0.0', port=port, debug=False)
